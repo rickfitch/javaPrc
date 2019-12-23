2419006fabfd81d9da1870f52c864936be5c827a
rickfitch <rickfitch@users.noreply.github.com>
2019-12-23 14:34:21 -0800

    Update version

--- javaPrc/src/app/Person.java
+++ javaPrc/src/app/Person.java
@@ -7,35 +7,29 @@ public class Person {
 
     private String name;
     private int age;
-
-    
+    // The following were all constructed in 
+    // 'source action'    
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
     @Override
     public String toString() {
         return "Person [age=" + age + ", name=" + name + "]";
     }
-
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -44,7 +38,6 @@ public class Person {
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
